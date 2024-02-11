cc7855657df3f3ce5a89a1ff17b073962a4fe860
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2024-02-11 22:35:27 +0100

    Implement atom enum macro

---
+++ lib/atom_enum.ex
@@ -0,0 +1,32 @@
+defmodule AtomEnum do
+  defmacro __using__(values: values) do
+    quote bind_quoted: [values: values] do
+      @type t :: unquote(Enum.reduce(values, fn e, acc -> {:|, [], [e, acc]} end))
+
+      def values do
+        unquote(values)
+      end
+
+      @spec cast(String.t() | atom()) :: {:ok, t()} | :error
+      def cast(value) when is_binary(value) do
+        value
+        |> String.to_existing_atom()
+        |> cast()
+      rescue
+        ArgumentError -> :error
+      end
+
+      def cast(atom) when atom in unquote(values) do
+        {:ok, atom}
+      end
+
+      def cast(atom) when is_atom(atom), do: :error
+
+      for value <- values do
+        def unquote(value)() do
+          unquote(value)
+        end
+      end
+    end
+  end
+end
