de0d527c0011dad377e61917e8c16cd9841bf0c0
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2024-06-07 00:16:37 +0200

    Add data for seeding

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Needlist.Repo
+alias Needlist.Repo.Want
+alias Needlist.Repo.Pagination
+
+{:ok, %Pagination{items: items}} =
+  "priv/repo/fixtures/wants.json"
+  |> File.read!()
+  |> Jason.decode!()
+  |> then(&Pagination.parse(&1, :wants, Want))
+
+Enum.each(items, &Repo.insert!/1)
