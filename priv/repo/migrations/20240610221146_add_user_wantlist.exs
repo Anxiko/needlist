7ac2a214097234e7d1663cb19a68a6b993c0a11e
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2024-06-12 00:01:21 +0200

    Fix want_id column name in many to many table

--- priv/repo/migrations/20240610221146_add_user_wantlist.exs
+++ priv/repo/migrations/20240610221146_add_user_wantlist.exs
@@ -4,7 +4,7 @@ defmodule Needlist.Repo.Migrations.AddUserWantlist do
   def change do
     create table(:user_wantlist, primary_key: false) do
       add :user_id, references(:users), primary_key: true
-      add :wantlist_id, references(:wants), primary_key: true
+      add :want_id, references(:wants), primary_key: true
     end
   end
 end
