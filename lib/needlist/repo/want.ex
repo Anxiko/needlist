ee85e20cc0457a11e9e5fd0304a4d9f38360c55e
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2024-06-21 01:14:00 +0200

    Implement wants context to get needlist page from repo

--- lib/needlist/repo/want.ex
+++ lib/needlist/repo/want.ex
@@ -35,7 +35,8 @@ defmodule Needlist.Repo.Want do
           basic_information: BasicInformation.t() | nil
         }
 
-  @type order() :: :asc | :desc
+  @type sort_order() :: :asc | :desc
+  @type sort_key() :: :artist | :title | :label | :added
 
   @spec changeset(t() | Changeset.t(t()), map()) :: Changeset.t(t())
   @spec changeset(map()) :: Changeset.t(t())
@@ -61,33 +62,50 @@ defmodule Needlist.Repo.Want do
     |> select([w, _u], w)
   end
 
-  @spec sort_by_artists(Ecto.Query.t() | __MODULE__, order()) :: Ecto.Query.t()
-  @spec sort_by_artists(order()) :: Ecto.Query.t()
+  @spec in_user_needlist_by_username(Ecto.Query.t() | __MODULE__, String.t()) :: Ecto.Query.t()
+  @spec in_user_needlist_by_username(String.t()) :: Ecto.Query.t()
+  def in_user_needlist_by_username(query \\ __MODULE__, username) do
+    query
+    |> join(:inner, [w], u in assoc(w, :users))
+    |> where([_w, u], u.username == ^username)
+    |> select([w, _u], w)
+  end
+
+  @spec sort_by_artists(Ecto.Query.t() | __MODULE__, sort_order()) :: Ecto.Query.t()
+  @spec sort_by_artists(sort_order()) :: Ecto.Query.t()
   def sort_by_artists(query \\ __MODULE__, order) do
     query
     |> Ecto.Query.order_by([{^order, :display_artists}])
   end
 
-  @spec sort_by_title(Ecto.Query.t() | __MODULE__, order()) :: Ecto.Query.t()
-  @spec sort_by_title(order()) :: Ecto.Query.t()
+  @spec sort_by_title(Ecto.Query.t() | __MODULE__, sort_order()) :: Ecto.Query.t()
+  @spec sort_by_title(sort_order()) :: Ecto.Query.t()
   def sort_by_title(query \\ __MODULE__, order) do
     query
     |> Ecto.Query.order_by([w], [{^order, fragment("?->>?", w.basic_information, "title")}])
   end
 
-  @spec sort_by_labels(Ecto.Query.t() | __MODULE__, order()) :: Ecto.Query.t()
-  @spec sort_by_labels(order()) :: Ecto.Query.t()
+  @spec sort_by_labels(Ecto.Query.t() | __MODULE__, sort_order()) :: Ecto.Query.t()
+  @spec sort_by_labels(sort_order()) :: Ecto.Query.t()
   def sort_by_labels(query \\ __MODULE__, order) do
     query
     |> Ecto.Query.order_by([{^order, :display_labels}])
   end
 
-  @spec sort_by_date_added(Ecto.Query.t() | __MODULE__, order()) :: Ecto.Query.t()
-  @spec sort_by_date_added(order()) :: Ecto.Query.t()
+  @spec sort_by_date_added(Ecto.Query.t() | __MODULE__, sort_order()) :: Ecto.Query.t()
+  @spec sort_by_date_added(sort_order()) :: Ecto.Query.t()
   def sort_by_date_added(query \\ __MODULE__, order) do
     query
     |> Ecto.Query.order_by([{^order, :date_added}])
   end
+
+  @spec sort_by(Ecto.Query.t() | __MODULE__, sort_key(), sort_order()) :: Ecto.Query.t()
+  @spec sort_by(sort_key(), sort_order()) :: Ecto.Query.t()
+  def sort_by(query \\ __MODULE__, sort_key, sort_order)
+  def sort_by(query, :artist, sort_order), do: sort_by_artists(query, sort_order)
+  def sort_by(query, :title, sort_order), do: sort_by_labels(query, sort_order)
+  def sort_by(query, :label, sort_order), do: sort_by_labels(query, sort_order)
+  def sort_by(query, :added, sort_order), do: sort_by_date_added(query, sort_order)
 
   defp compute_sorting_fields(%Ecto.Changeset{valid?: true} = changeset) do
     changeset
@@ -101,5 +119,12 @@ defmodule Needlist.Repo.Want do
       _ ->
         changeset
     end
+  end
+
+  @spec paginated(Ecto.Query.t() | __MODULE__, pos_integer(), pos_integer()) :: Ecto.Query.t()
+  def paginated(query \\ __MODULE__, page, per_page) do
+    query
+    |> limit(per_page)
+    |> offset(per_page * (page - 1))
   end
 end
---
+++ lib/needlist/wants.ex
@@ -0,0 +1,29 @@
+defmodule Needlist.Wants do
+  alias Needlist.Repo
+  alias Needlist.Repo.Want
+
+  @type sort_key() :: Want.sort_key()
+  @type sort_order() :: Want.sort_order()
+
+  @type needlist_page_options() :: [
+          page: pos_integer(),
+          per_page: pos_integer(),
+          sort: sort_key(),
+          sort_order: sort_order()
+        ]
+
+  @spec get_needlist_page(String.t(), needlist_page_options()) :: [Want.t()]
+  @spec get_needlist_page(String.t()) :: [Want.t()]
+  def get_needlist_page(username, options \\ []) do
+    page = Keyword.get(:page, 1)
+    per_page = Keyword.get(:per_page, 50)
+    sort_key = Keyword.get(:sort, :label)
+    sort_order = Keyword.get(:sort_order, :asc)
+
+    Want
+    |> Want.in_user_needlist_by_username(username)
+    |> Want.sort_by(sort_key, sort_order)
+    |> Want.paginated(page, per_page)
+    |> Repo.all()
+  end
+end
