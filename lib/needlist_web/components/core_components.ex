160a12af1f5af5e269a8047e922e661885e7bc48
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2025-03-28 00:04:47 +0100

    Show button as disabled when it is

--- lib/needlist_web/components/core_components.ex
+++ lib/needlist_web/components/core_components.ex
@@ -232,7 +232,7 @@ defmodule NeedlistWeb.CoreComponents do
       type={@type}
       class={[
         "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
-        "text-sm font-semibold leading-6 text-white active:text-white/80",
+        "text-sm font-semibold leading-6 text-white active:text-white/80 disabled:cursor-not-allowed",
         @class
       ]}
       {@rest}
