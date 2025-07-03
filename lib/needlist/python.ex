05a6a7b3a4b33c3c045b17acb0ef2fefdc6ec45d
Joaquín <Kindos7@gmail.com>
2025-07-03 18:53:00 +0200

    Use single Python interpreter for listings scraping (#41)
    * Use single Python interpreter for listings scraping
    
    * Add missing spec

--- lib/needlist/python.ex
+++ lib/needlist/python.ex
@@ -3,26 +3,60 @@ defmodule Needlist.Python do
   Interop with Python scripts
   """
 
+  use GenServer
+
   alias Nullables.Result
 
   @spec scrape_listings(integer()) :: Result.result(String.t())
   def scrape_listings(release_id) do
-    case :python.start(python_path: String.to_charlist(python_path())) do
-      {:ok, pid} ->
-        do_scrape_listings(pid, release_id)
+    GenServer.call(__MODULE__, {:scrape_listings, release_id})
+  end
 
-      _ ->
-        {:error, :python_process}
+  @spec start_link(keyword()) :: GenServer.on_start()
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_args) do
+    case start_python() do
+      {:ok, pid} ->
+        {:ok, pid}
+
+      {:error, reason} ->
+        {:stop, reason}
     end
   end
 
-  @spec do_scrape_listings(pid(), integer()) :: Result.result(String.t())
+  @impl true
+  def terminate(_reason, pid) do
+    :python.stop(pid)
+  end
+
+  @impl true
+  def handle_call({:scrape_listings, release_id}, _from, pid) do
+    pid
+    |> do_scrape_listings(release_id)
+    |> case do
+      {:reply, response} ->
+        {:reply, response, pid}
+
+      {:error, reason} ->
+        {:stop, reason, {:error, reason}, pid}
+    end
+  end
+
+  defp start_python do
+    :python.start_link(python_path: String.to_charlist(python_path()))
+  end
+
+  @spec do_scrape_listings(pid(), integer()) :: {:reply, Result.result(String.t())} | {:error, any()}
   defp do_scrape_listings(pid, release_id) do
     pid
     |> :python.call(:discogs_scraper, :scrape_listings, [release_id])
     |> case do
-      {~c"error", status_code} -> {:error, {:status_code, status_code}}
-      {~c"ok", response} -> {:ok, to_string(response)}
+      {~c"error", status_code} -> {:reply, {:error, {:status_code, status_code}}}
+      {~c"ok", response} -> {:reply, {:ok, to_string(response)}}
     end
   rescue
     e in ErlangError ->
